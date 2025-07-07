54352b4740d54b3751eb1669fe87cc6df9fa767b
Amphivinut <youjerr7@gmail.com>
2025-07-06 19:55:36 -0700

    re-commit

--- main.lua
+++
@@ -1,4 +0,0 @@
-assert(SMODS.load_file('items/consumable.lua'))()
-assert(SMODS.load_file('items/deck.lua'))()
-assert(SMODS.load_file('items/enhancement.lua'))()
-assert(SMODS.load_file('items/joker.lua'))()
