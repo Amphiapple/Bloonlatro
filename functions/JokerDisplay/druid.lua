08402c1e20026fdbc0c1e9e4a80c92b23e5d7cf6
Amphivinut <youjerr7@gmail.com>
2026-05-06 13:14:45 -0700

    v0.4.5 some druids joker display

--- functions/JokerDisplay/druid.lua
+++ functions/JokerDisplay/druid.lua
@@ -1,64 +1,117 @@
---[[
 JokerDisplay.Definitions["j_bloons_druid"] = { --Druid
-    text = {
-        { text = "+", colour = G.C.MULT },
-        { ref_table = "card.joker_display_values", ref_value = "mult", colour = G.C.MULT },
-    },
-    reminder_text = {
-        { text = "(" },
-        { text = "Full House", colour = G.C.ORANGE },
-        { text = ")" }
-    },
-    calc_function = function(card)
-        local mult = 0
-        local _, poker_hands, _ = JokerDisplay.evaluate_hand()
-        if poker_hands['Full House'] and next(poker_hands['Full House']) then
-            mult = card.ability.extra.mult
-        end
-        card.joker_display_values.mult = mult
-    end
+}
+
+JokerDisplay.Definitions["j_bloons_hard_thorns"] = { --Hard Thorns
 }
 
 JokerDisplay.Definitions["j_bloons_heart_of_thunder"] = { --Heart of Thunder
-    extra = {
-        {
-            { text = "(", colour = G.C.GREEN, scale = 0.3 },
-            { ref_table = "card.joker_display_values", ref_value = "odds", colour = G.C.GREEN, scale = 0.3 },
-            { text = ")", colour = G.C.GREEN, scale = 0.3 },
-        }
-    },
-    calc_function = function(card)
-        local numerator, denominator = SMODS.get_probability_vars(card, card.ability.extra.num, card.ability.extra.denom, 'thunder')
-        card.joker_display_values.odds = numerator .. " in " .. denominator
-    end
 }
 
 JokerDisplay.Definitions["j_bloons_druid_of_the_storm"] = { --Druid of the Storm
     text = {
-        { text = "+", colour = G.C.BLUE },
-        { ref_table = "card.joker_display_values", ref_value = "hands", colour = G.C.BLUE }
+        { text = "+" },
+        { ref_table = "card.joker_display_values", ref_value = "planets" }
     },
+    text_config = { colour = G.C.SECONDARY_SET.Planet },
     reminder_text = {
         { text = "(" },
-        { ref_table = "card.joker_display_values", ref_value = "active" },
-        { text = ")" },
+        { ref_table = "card.joker_display_values", ref_value = "poker_hand", colour = G.C.ORANGE },
+        { text = ")" }
+    },
+    extra = {
+        {
+            { text = "(" },
+            { ref_table = "card.joker_display_values", ref_value = "odds" },
+            { text = ")" },
+        }
+    },
+    extra_config = { colour = G.C.GREEN, scale = 0.3 },
+    calc_function = function(card)
+        local planet = nil
+        for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+            if v.config.hand_type == JokerDisplay.evaluate_hand() then
+                planet = v.name
+            end
+        end
+        local n, d = SMODS.get_probability_vars(card, card.ability.extra.num, card.ability.extra.denom, 'druid_of_the_storm')
+        card.joker_display_values.odds = n .. " in " .. d
+        card.joker_display_values.poker_hand = planet or 'None'
+        card.joker_display_values.planets = planet and 1 or 0
+    end
+}
+
+JokerDisplay.Definitions["j_bloons_ball_lightning"] = { --Ball Lightning
+    text = {
+        {
+            border_nodes = {
+                { text = "X" },
+                { ref_table = "card.joker_display_values", ref_value = "Xmult" }
+            }
+        }
     },
     calc_function = function(card)
-        if card.ability.extra.counter == card.ability.extra.limit - 1 then
-            card.joker_display_values.hands = card.ability.extra.hands
-            card.joker_display_values.active = "Next!"
-        else
-            card.joker_display_values.hands = 0
-            card.joker_display_values.active = card.ability.extra.limit - card.ability.extra.counter % card.ability.extra.limit - 1 .. " remaining"
+        local text, _, _ = JokerDisplay.evaluate_hand()
+        local total_Xmult = 1
+        for _, consumable in ipairs(G.consumeables.cards) do
+            local center_key = consumable.config and consumable.config.center_key
+            if center_key and G.P_CENTERS[center_key] and G.P_CENTERS[center_key].set == "Planet" then
+                local hand_type = G.P_CENTERS[center_key].config.hand_type
+                total_Xmult = total_Xmult * (hand_type == text and card.ability.extra.Xmult_match or card.ability.extra.Xmult)
+            end
         end
+        card.joker_display_values.Xmult = total_Xmult
     end
+}
+
+JokerDisplay.Definitions["j_bloons_monarch_of_storms"] = { --Monarch of Storms
+    text = {
+        { text = "+" },
+        { ref_table = "card.joker_display_values", ref_value = "planets" }
+    },
+    text_config = { colour = G.C.SECONDARY_SET.Planet },
+    reminder_text = {
+        { text = "(" },
+        { ref_table = "card.joker_display_values", ref_value = "poker_hand", colour = G.C.ORANGE },
+        { text = ")" }
+    },
+    extra_config = { colour = G.C.GREEN, scale = 0.3 },
+    calc_function = function(card)
+        local planet = nil
+        for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+            if v.config.hand_type == G.GAME.last_hand_played then
+                planet = v.name
+            end
+        end
+        card.joker_display_values.poker_hand = planet or 'None'
+        card.joker_display_values.planets = planet and 1 or 0
+    end
+}
+
+JokerDisplay.Definitions["j_bloons_thorn_swarm"] = { --Thorn Swarm
+}
+
+JokerDisplay.Definitions["j_bloons_heart_of_oak"] = { --Heart of Oak
+    text = {
+        { text = "+$" },
+        { ref_table = "card.ability.extra", ref_value = "money" },
+    },
+    text_config = { colour = G.C.MONEY }
+}
+
+JokerDisplay.Definitions["j_bloons_druid_of_the_jungle"] = { --Druid of the Jungle
+    text = {
+        { text = "+$" },
+        { ref_table = "card.joker_display_values", ref_value = "money" },
+    },
+    text_config = { colour = G.C.MONEY }
 }
 
 JokerDisplay.Definitions["j_bloons_jungles_bounty"] = { --Jungle's Bounty
     text = {
-        { text = "+$", colour = G.C.MONEY },
-        { ref_table = "card.joker_display_values", ref_value = "money", colour = G.C.MONEY },
+        { text = "+$" },
+        { ref_table = "card.joker_display_values", ref_value = "money" },
     },
+    text_config = { colour = G.C.MONEY },
     reminder_text = {
         { text = "(" },
         { text = "Full House", colour = G.C.ORANGE },
@@ -98,4 +151,3 @@ JokerDisplay.Definitions["j_bloons_avatar_of_wrath"] = { --Avatar of Wrath
         }
     }
 }
-]]
